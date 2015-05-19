4eeda73f0f8312894d3d2975164f3baf970a87fc
abdoulkarim <abdoulkarimsow24@gmail.com>
2015-05-19 11:14:01 +0000

    Revert "Change Ebola figures to ORS figures"
    This reverts commit 5666224752365f7f202257416530e912b314c4f5.

--- README.md
+++
@@ -1,6 +0,0 @@
-Ebola_Dashboard
-===============
-
-A dashboard to explore data surrounding the Ebola outbreak
-
-http://simonbjohnson.github.io/Ebola_Dashboard/
--- data/KeyStats.js
+++
@@ -1,6 +0,0 @@
-var keyStats = 
-        {"Total":{"population":"21.7 mil"},
-	"Guinea":{"population":"11.5 mil"},
-	"Liberia":{"population":"4.2 mil",},
-	"Sierra Leone":{"population":"6.0 mil"},
-};		
